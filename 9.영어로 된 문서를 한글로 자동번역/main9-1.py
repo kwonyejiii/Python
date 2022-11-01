1bdd19966478a7b338b8aba434ec3f7731cc25fa
권예지 <110161674+kwonyejiii@users.noreply.github.com>
2022-11-02 00:36:46 +0900

    22-11-02

---
+++ 9.영어로 된 문서를 한글로 자동번역/main9-1.py
@@ -0,0 +1,14 @@
+# 영어로 된 문서를 한글로 자동변역
+
+from unittest import result
+import googletrans
+
+translator = googletrans.Translator()
+
+str1 ="행복하세요"
+result1 =translator.translate(str1, dest='en', src='auto')
+print(f"행복하세요=>{result1.text}")
+
+str2 ="I am happy"
+result2 = translator.translate(str2, dest='ko', src='en')
+print(f"I am happy => {result2.text}")
